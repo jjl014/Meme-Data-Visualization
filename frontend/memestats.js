c87f518a8c435163be54e24c662a3130582559a9
Jimmy Li <jimmyli921@gmail.com>
2017-10-19 17:11:40 -0700

    fix memestats db

--- frontend/memestats.js
+++ frontend/memestats.js
@@ -7,12 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
   window.selectByPopular = selectByPopular;
   window.updatePopularMemes = updatePopularMemes;
   // window.setTimeout(() => {
-  //   for(let i = 0; i < 4; i++) {
-  //     updatePopularMemes(i.toString(), "");
-  //   }
-  // }, 1000);
-
-  // getMemeImages();
+    for(let i = 0; i < 4; i++) {
+      updatePopularMemes(i.toString(), "");
+    }
+  // }
+  getMemeImages();
 
   const images = {};
   buildMemeChart(images);
